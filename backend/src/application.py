78ae7a2da2784c00a12f1923168a36eb826b23bc
gpuzikov <bythetort@gmail.com>
2024-10-05 00:59:07 +0300

    added logger

--- backend/src/application.py
+++ backend/src/application.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import FastAPI
 from src.api.base_route import router as base_router
 
@@ -6,3 +8,15 @@ def create_app():
     application = FastAPI()
     application.include_router(base_router)
     return application
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+log = logging.getLogger(__name__)
