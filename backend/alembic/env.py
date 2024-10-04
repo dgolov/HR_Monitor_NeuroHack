c1aaa58030b0ffea0affb2719f17b88a0ce8cbfc
Дмитрий Голов <dgolov@mts.ru>
2024-10-05 02:30:52 +0300

    added Dockerfile for ui

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,6 +7,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+import os
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 app_config = context.config
--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,7 +1,9 @@
-import os
-
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
+
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
@@ -10,10 +12,7 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         extra="allow",
     )
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    DATABASE_URL: str = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db/{POSTGRES_DB}"
+    DATABASE_URL: str
 
 
 app_config = Settings()
