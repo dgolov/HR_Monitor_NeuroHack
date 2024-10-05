96ef300fc10b5215519dc2288e540e3d81ec0780
Александр Мамедов <anifan01@gmail.com>
2024-10-05 12:23:24 +0300

    reformat

--- backend/src/api/v1/vacancy_files/routes.py
+++ backend/src/api/v1/vacancy_files/routes.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
+
 from src.api.v1.deps import repo_dep
-from src.schema import schemas
 from src.repositiry.repo import Repository
+from src.schema import schemas
 from src.settings import logger
 
 
-router = APIRouter(prefix="/vacancy_files")
+router = APIRouter(prefix="/vacancy-files")
 
 
 @router.post("/")
 async def create_vacancy_file(
-        data: schemas.VacancyFileCreate,
-        repository: Repository = repo_dep
-):
-    logger.debug(f'Create vacancy file - {data}')
+    data: schemas.VacancyFileCreate,
+    repository: Repository = repo_dep,
+) -> JSONResponse:
+    logger.debug(f"Create vacancy file - {data}")
     await repository.create_vacancy_file(vacancy_file=data)
     return JSONResponse(
         content={"message": "Ok"},
-        status_code=status.HTTP_201_CREATED
+        status_code=status.HTTP_201_CREATED,
     )
