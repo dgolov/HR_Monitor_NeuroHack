684cc38446bb6cf2a817834138d3f54bf245dfa6
Александр Мамедов <anifan01@gmail.com>
2024-10-05 09:52:39 +0300

    typing

--- backend/src/repositiry/repo.py
+++ backend/src/repositiry/repo.py
@@ -1,17 +1,21 @@
+from typing import Any
+
+from fastapi import HTTPException
+from sqlalchemy import Select, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.database import models
 from src.schema import schemas
 from src.settings import logger
-from fastapi import HTTPException
-from sqlalchemy import select
-from typing import List
 
 
 class RepositoryBase:
-    """ Базовый класс обращения в БД """
-    def __init__(self, session):
+    """Базовый класс обращения в БД."""
+
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def _all(self, query):
+    async def _all(self, query: Select) -> list[Any]:
         query_result = await self.session.execute(query)
         rows = query_result.all()
         return [data[0] for data in rows if data[0]]
@@ -28,7 +32,7 @@ class RepositoryBase:
     async def _one(result):
         return result.one()
 
-    async def _insert_one(self, model: models.Base) -> None:
+    async def _insert_one(self, model: Any) -> None:
         try:
             self.session.add(model)
             await self.session.commit()
@@ -36,14 +40,14 @@ class RepositoryBase:
             logger.error(f"Add {model.__tablename__} error - {e}")
             raise HTTPException(status_code=400, detail="Bad request")
 
-    async def _update(self, obj, data):
+    async def _update(self, obj, data) -> None:
         if not isinstance(data, dict):
             data = data.dict()
         for field, value in data.items():
             setattr(obj, field, value)
         await self.session.commit()
 
-    async def _delete(self, obj):
+    async def _delete(self, obj) -> None:
         await self.session.delete(obj)
         await self.session.commit()
 
@@ -67,7 +71,7 @@ class Repository(RepositoryBase):
         query = select(self.user)
         return await self._all(query=query)
 
-    async def create_user(self, user: schemas.UserCreate):
+    async def create_user(self, user: schemas.UserCreate) -> None:
         user_model = models.User(**user.model_dump())
         self.session.add(user_model)
         await self.session.commit()
@@ -75,8 +79,8 @@ class Repository(RepositoryBase):
     async def get_vacancies(self) -> List[models.Vacancy]:
         query = select(self.vacancy)
         return await self._all(query=query)
-    
-    async def create_vacancy(self, vacancy: schemas.VacancyCreate):
+
+    async def create_vacancy(self, vacancy: schemas.VacancyCreate) -> None:
         vacancy_model = models.Vacancy(**vacancy.model_dump())
         self.session.add(vacancy_model)
         await self.session.commit()
@@ -84,8 +88,8 @@ class Repository(RepositoryBase):
     async def get_vacancy_files(self) -> List[models.VacancyFile]:
         query = select(self.vacancy_file)
         return await self._all(query=query)
-            
-    async def create_vacancy_file(self, vacancy_file: schemas.VacancyFileCreate):
+
+    async def create_vacancy_file(self, vacancy_file: schemas.VacancyFileCreate) -> None:
         vacancy_file_model = models.VacancyFile(**vacancy_file.model_dump())
         self.session.add(vacancy_file_model)
         await self.session.commit()
@@ -93,8 +97,8 @@ class Repository(RepositoryBase):
     async def get_interviews(self) -> List[models.Interview]:
         query = select(self.interview)
         return await self._all(query=query)
-    
-    async def create_interview(self, interview: schemas.InterviewCreate):
+
+    async def create_interview(self, interview: schemas.InterviewCreate) -> None:
         interview_model = models.Interview(**interview.model_dump())
         self.session.add(interview_model)
         await self.session.commit()
@@ -102,8 +106,8 @@ class Repository(RepositoryBase):
     async def get_candidates(self) -> List[models.Candidate]:
         query = select(self.candidate)
         return await self._all(query=query)
-    
-    async def create_candidate(self, candidate: schemas.CandidateCreate):
+
+    async def create_candidate(self, candidate: schemas.CandidateCreate) -> None:
         candidate_model = models.Candidate(**candidate.model_dump())
         self.session.add(candidate_model)
         await self.session.commit()
