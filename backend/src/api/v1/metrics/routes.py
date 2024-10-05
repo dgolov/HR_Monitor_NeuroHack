e83c12a93db1c2ac5eb638b5fe40fb12710318a7
Александр Мамедов <anifan01@gmail.com>
2024-10-05 11:42:28 +0300

    fix average_hire_time

--- backend/src/api/v1/metrics/routes.py
+++ backend/src/api/v1/metrics/routes.py
@@ -1,19 +1,18 @@
-from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter
+
 from src.api.v1.deps import repo_dep
-from src.schema import schemas
 from src.repositiry.repo import Repository
-from src.settings import logger
+from src.schema import schemas
 
 
 router = APIRouter(prefix="/metrics")
 
 
-@app.get("/vacancies/average_hire_time")
-def average_hire_time(repository: Repository = repo_dep):
+@router.get("/average_hire_time")
+def average_hire_time(repository: Repository = repo_dep) -> schemas.VacancyAverageTimeResponse:
     # Группируем вакансии по году и месяцу закрытия
 
-    grouped_vacancies = repo_dep.get_grouped_vacancies
+    grouped_vacancies = repository.get_grouped_vacancies
     result = {}
     for entry in grouped_vacancies:
         year = entry.year
@@ -26,7 +25,7 @@ def average_hire_time(repository: Repository = repo_dep):
         # Добавляем данные по месяцам
         result[year][month] = {
             "average_closure_time_in_days": entry.average_closure_time,
-            "vacancies_count": entry.vacancies_count
+            "vacancies_count": entry.vacancies_count,
         }
 
     return schemas.VacancyAverageTimeListResponse(data=result)
