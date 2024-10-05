66c6376e5fa655e60d4a38cf86b6c108b0a6b3fd
Александр Мамедов <anifan01@gmail.com>
2024-10-05 12:59:54 +0300

    reformat

--- backend/src/__main__.py
+++ backend/src/__main__.py
@@ -1,5 +1,7 @@
 import uvicorn
-from src.application import (create_app)
+
+from src.application import create_app
+
 
 if __name__ == "__main__":
     uvicorn.run(create_app(), host="0.0.0.0", port=8000)
