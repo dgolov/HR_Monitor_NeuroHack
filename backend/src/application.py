53385384ae8959934b7cee8e0c6b6f3248b5f91c
Александр Мамедов <anifan01@gmail.com>
2024-10-05 12:59:26 +0300

    typing

--- backend/src/application.py
+++ backend/src/application.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+
 from src.api.base_route import router as base_router
 from src.settings import logger
 
 
-def create_app():
-    logger.info('Start backend')
+def create_app() -> FastAPI:
+    logger.info("Start backend")
     application = FastAPI()
     application.include_router(base_router)
     return application
