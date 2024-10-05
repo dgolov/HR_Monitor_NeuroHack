c0111f5ca12a73d444605d9f87a95746c6d21b75
Александр Мамедов <anifan01@gmail.com>
2024-10-05 10:57:24 +0300

    fix get_candidates

--- backend/src/api/v1/candidates/routes.py
+++ backend/src/api/v1/candidates/routes.py
@@ -5,7 +5,6 @@ from src.api.v1.deps import repo_dep
 from src.repositiry.repo import Repository
 from src.schema import schemas
 from src.settings import logger
-from src.database import models
 
 
 router = APIRouter(prefix="/candidates")
@@ -26,8 +25,9 @@ async def create_candidate(
 
 @router.get("/candidates/{vacancy_id}")
 async def get_candidates(
-        vacancy_id: int
-):
-    log.debug(f'route /candidates/ called with {vacancy_id=}')
-    candidates_list: List[models.Candidate] = await repository.get_candidates(vacancy_id=vacancy_id)
-    return list(schemas.Candidate.from_orm(candidate) for candidate in candidates_list)
+    vacancy_id: int,
+    repository: Repository = repo_dep,
+) -> list[schemas.Candidate]:
+    logger.debug(f"route /candidates/ called with {vacancy_id=}")
+    candidates_list = await repository.get_candidates(vacancy_id=vacancy_id)
+    return [schemas.Candidate.model_validate(candidate) for candidate in candidates_list]
