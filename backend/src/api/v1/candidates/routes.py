b3ccc1f69b170f776095d82a7bd95f27331d2a41
Александр Мамедов <anifan01@gmail.com>
2024-10-05 10:08:26 +0300

    add types

--- backend/src/api/v1/candidates/routes.py
+++ backend/src/api/v1/candidates/routes.py
@@ -1,31 +1,31 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
+
 from src.api.v1.deps import repo_dep
-from src.schema import schemas
 from src.repositiry.repo import Repository
+from src.schema import schemas
 from src.settings import logger
 
 
 router = APIRouter(prefix="/candidates")
 
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_candidate(
-        data: schemas.CandidateCreate,
-        repository: Repository = repo_dep
-):
-    logger.debug(f'Create candidate - {data}')
+    data: schemas.CandidateCreate,
+    repository: Repository = repo_dep,
+) -> JSONResponse:
+    logger.debug(f"Create candidate - {data}")
     await repository.create_candidate(candidate=data)
     return JSONResponse(
         content={"message": "Ok"},
-        status_code=status.HTTP_201_CREATED
+        status_code=status.HTTP_201_CREATED,
     )
 
 
 @router.get("/candidates/{vacancy_id}")
 async def get_candidates(
-        vacancy_id: int
-):
-    log.debug(f'route /candidates/ called with {vacancy_id=}')
-    pass  # todo
-
+    vacancy_id: int,
+) -> list[schemas.Candidate]:
+    logger.debug(f"route /candidates/ called with {vacancy_id=}")
+    # TODO
