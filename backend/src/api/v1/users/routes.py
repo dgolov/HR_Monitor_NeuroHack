758470e663c8269eafac6dfcaf0145cf8e756d88
Дмитрий Голов <dgolov@mts.ru>
2024-10-06 03:41:54 +0300

    added role filter

--- backend/src/api/v1/users/routes.py
+++ backend/src/api/v1/users/routes.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
-
 from src.api.v1.deps import repo_dep
 from src.repositiry.repo import Repository
 from src.schema import schemas
 from src.settings import logger
+from typing import Optional
 
 
 router = APIRouter(prefix="/users")
 
 
 @router.get("/")
-async def get_users(repository: Repository = repo_dep) -> list[schemas.User]:
-    users_list = await repository.get_users()
+async def get_users(repository: Repository = repo_dep, role: Optional[str] = None) -> list[schemas.User]:
+    users_list = await repository.get_users(role=role)
     return [schemas.User.model_validate(user) for user in users_list]
 
 
