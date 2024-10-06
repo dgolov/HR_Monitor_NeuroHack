7e89af0fa0c0027d4f3a51d251afbfb78eadcd0e
Дмитрий Голов <dgolov@mts.ru>
2024-10-06 11:40:10 +0300

    bf

--- ui/hr-metrics-app/src/main.js
+++ ui/hr-metrics-app/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue';
 import router from "@/routes/index";
-import DefaultLayout from "./layouts/DefaultLayout";
+import DefaultLayout from "@/layouts/DefaultLayout";
 
 
 const app = createApp(App);
